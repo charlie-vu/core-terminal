e5550d63b65116b118458d2489cd43b633ec0681
Charlie <vanvu25894@gmail.com>
2024-10-17 12:48:38 +0700

    feat: Partners - draft

--- app/components/Partners.jsx
+++ app/components/Partners.jsx
@@ -27,15 +27,17 @@ export default function Partners() {
         <section className="partners py-4 py-lg-5">
             <div className="bg"></div>
             <h4 className="text-center display-5 fw-500">Partners & <span className="text-primary">Investors</span></h4>
-            <div className="mt-4 mt-lg-5 mt-xxl-5 d-flex justify-content-center gap-4 flex-wrap">
+            <Row className="mt-4 mt-lg-5 mt-xxl-5 justify-content-center g-3 g-lg-4 g-xxl-4">
                 {partnerList.map((item, i) =>
-                    <div key={`partner-${i}`} className="partner-item border-rainbow rainbow-hz rounded-4 text-center">
-                        <div className="rounded-3 overflow-hidden p-4">
-                            <Image src={item.img} alt={item.name} />
+                    <Col key={`partner-${i}`} xs={6} md={4}>
+                        <div className="partner-item border-rainbow rainbow-hz rounded-4 text-center">
+                            <div className="rounded-3 overflow-hidden p-3 p-md-4 px-lg-5 py-xxl-4">
+                                <Image src={item.img} alt={item.name} className="col-12 col-xxl-auto" />
+                            </div>
                         </div>
-                    </div>
+                    </Col>
                 )}
-            </div>
+            </Row>
         </section>
     )
 }
